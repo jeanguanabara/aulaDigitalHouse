8d06e182c04f21815630450a4e984654bbe48303
Jean Guanabara <112223294+jeanguanabara@users.noreply.github.com>
2022-09-01 17:28:30 -0300

    Update personagensResultadoCorreto.js
    esta duplicado o código

--- exIntegradoresModulo1/personagensResultadoCorreto.js
+++ exIntegradoresModulo1/personagensResultadoCorreto.js
@@ -1,4 +1,3 @@
-
 function filme(personagens, filmes, lancamentos, id){
     if (((id-1)<=personagens.length) && ((id-1)<=filmes.length) && ((id-1)<=lancamentos.length)){
         return personagens[id-1] + " é um personagem do filme " + filmes[id-1] + " que estreou no cinema em " + lancamentos[id-1] + "."
@@ -8,12 +7,4 @@ function filme(personagens, filmes, lancamentos, id){
 
     }
 
-function filme(personagens, filmes, lancamentos, id){
-    if (((id-1)<=personagens.length) && ((id-1)<=filmes.length) && ((id-1)<=lancamentos.length)){
-        return personagens[id-1] + " é um personagem do filme " + filmes[id-1] + " que estreou no cinema em " + lancamentos[id-1] + "."
-    }else{
-        return "Essa não é uma opção válida."
-    }
-
-    }
 
