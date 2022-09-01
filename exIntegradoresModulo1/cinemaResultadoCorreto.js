0836726edd155734cde15ee4f6d09367b551b968
Jean Guanabara <112223294+jeanguanabara@users.noreply.github.com>
2022-09-01 17:30:26 -0300

    estava duplicado o código

--- exIntegradoresModulo1/cinemaResultadoCorreto.js
+++ exIntegradoresModulo1/cinemaResultadoCorreto.js
@@ -17,21 +17,3 @@ function calculaGostos(notas){
     return resultado
 }
 
-function calculaGostos(notas){
-    
-    let primeiro = 0
-    let segundo= 0
-    let terceiro = 0
-    for( i=0; i<notas.length; i++){
-        if (notas[i] <= 1){
-            primeiro++
-        }else if (notas[i] >=2 && notas[i] <= 3){
-            segundo++
-        }else if (notas[i] >=4 && notas[i] <=5) {
-            terceiro++
-        }
-    }
-    let resultado = [primeiro, segundo, terceiro]
-    return resultado
-
-}
